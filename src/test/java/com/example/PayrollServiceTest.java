8981c1aa8631c4f08d11e94b81a0cb9dd103a106
Surendar P <Surendar P>
2017-12-22 14:56:25 +0530

    added test case for PayrollService.da

--- src/test/java/com/example/PayrollServiceTest.java
+++ src/test/java/com/example/PayrollServiceTest.java
@@ -25,5 +25,11 @@ public class PayrollServiceTest {
 		assertEquals(4000.00, payrollService.hra(10000.00),2);
 	}
 
+
+	@Test
+	public void daShouldBeTenPercentOfSalary() {
+		assertEquals(1000.00, payrollService.da(10000.00),2);
+	}
+
 	
 }
